789a44ce64ef0946dcc508ccbf6bf4de01624e4f
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-29 18:48:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -4,13 +4,6 @@ def rescue_princess_peach
 end
 
 def catch_phrase
+  phrase = "It's-a me, Mario!"
   puts phrase
-end
-
-require "spec_helper"
-describe "#catch_phrase" do 
-  it "puts out a catch phrase" do 
-    phrase = "It's-a me, Mario!"
-    expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
-  end
 end
